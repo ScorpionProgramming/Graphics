5f195a7c047f5ed55e15d3750ec3cbb1f29046f7
ScorpionProgramming <Scorpionprogramming@gmail.com>
2016-12-11 21:43:51 +0100

    Engine - Hello Triangle vom OpenGL Buch

---
+++ LearnOpenGL_glfw_glew/Images/RGBImage.cpp
@@ -0,0 +1,106 @@
+#include "RGBImage.h"
+
+RGBImage::RGBImage(unsigned int Width, unsigned int Height)
+{
+	m_Width = Width;
+	m_Height = Height;
+	m_Image = new Color[Width * Height];
+}
+
+RGBImage::~RGBImage()
+{
+	delete m_Image;
+}
+
+void RGBImage::setPixelColor(unsigned int x, unsigned int y, const Color & c)
+{
+	if(x >= 0 && x <= m_Width-1 && y >= 0 && y < m_Height)
+		m_Image[ x + y * m_Width] = c;
+}
+
+const Color & RGBImage::getPixelColor(unsigned int x, unsigned int y) const
+{
+	if (x >= 0 && x <= m_Width-1 && y >= 0 && y < m_Height)
+		return m_Image[ x + y * m_Width];
+}
+
+
+unsigned int RGBImage::width() const
+{
+	return m_Width;
+}
+
+unsigned int RGBImage::height() const
+{
+	return m_Height;
+}
+
+unsigned char RGBImage::convertColorChannel(float f)
+{
+	if (f<0) return (int)(0);
+	if (f>255) return (int)(255);
+	return (int)(round(f * 255));
+}
+
+bool RGBImage::saveToDisk(const char * Filename)
+{
+	int w = this->m_Width;
+	int h = this->m_Height;
+
+	FILE *f;
+	unsigned char *img = NULL;
+	int filesize = 54 + 3 * w*h;
+	if (img) free(img); // Ist das her überhaupt nötig?
+
+	img = (unsigned char *)malloc(filesize);
+	memset(img, 0, sizeof(img));
+
+	int x, y;
+
+	for (x = 0; x<w; x++)
+	{
+		for (y = 0; y<h; y++)
+		{
+			Color c = this->m_Image[y*this->m_Width + x];
+
+			img[(x + y*w) * 3 + 2] = this->convertColorChannel(c.R);
+			img[(x + y*w) * 3 + 1] = this->convertColorChannel(c.G);
+			img[(x + y*w) * 3 + 0] = this->convertColorChannel(c.B);
+		}
+	}
+
+	unsigned char bmpfileheader[14] = { 'B', 'M', 0, 0, 0, 0, 0, 0, 0, 0, 54, 0, 0, 0 };
+	unsigned char bmpinfoheader[40] = { 40, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 24, 0 };
+	unsigned char bmppad[3] = { 0, 0, 0 };
+
+	bmpfileheader[2] = (unsigned char)(filesize);
+	bmpfileheader[3] = (unsigned char)(filesize >> 8);
+	bmpfileheader[4] = (unsigned char)(filesize >> 16);
+	bmpfileheader[5] = (unsigned char)(filesize >> 24);
+
+	bmpinfoheader[4] = (unsigned char)(w);
+	bmpinfoheader[5] = (unsigned char)(w >> 8);
+	bmpinfoheader[6] = (unsigned char)(w >> 16);
+	bmpinfoheader[7] = (unsigned char)(w >> 24);
+	bmpinfoheader[8] = (unsigned char)(h);
+	bmpinfoheader[9] = (unsigned char)(h >> 8);
+	bmpinfoheader[10] = (unsigned char)(h >> 16);
+	bmpinfoheader[11] = (unsigned char)(h >> 24);
+
+	f = fopen(Filename, "wb");
+	fwrite(bmpfileheader, 1, 14, f);
+	fwrite(bmpinfoheader, 1, 40, f);
+
+	int i;
+	for (i = 0; i<h; i++)
+	{
+		fwrite(img + (w*(h - i - 1) * 3), 3, w, f);
+		fwrite(bmppad, 1, (4 - (w * 3) % 4) % 4, f);
+	}
+
+	fclose(f);
+	free(img);
+
+	return true;
+}
+
---
+++ LearnOpenGL_glfw_glew/main.cpp
@@ -0,0 +1,238 @@
+#include <glew.h>
+#include <glfw3.h>
+#include <iostream>
+
+//testweise
+#include <fstream>
+#include <string>
+
+// Function prototypes
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+
+/*GLfloat vertices[] = { -0.5f,-0.5f, 0.0f,
+						0.5f, -0.5f, 0.0f,
+						0.0f, 0.5f, 0.0f 
+					};
+					*/
+/*
+GLfloat vertices[] = {
+	// First triangle
+	0.5f, 0.5f, 0.0f, // Top Right
+	0.5f, -0.5f, 0.0f, // Bottom Right
+	-0.5f, 0.5f, 0.0f, // Top Left
+	// Second triangle
+	0.5f, -0.5f, 0.0f, // Bottom Right
+	-0.5f, -0.5f, 0.0f, // Bottom Left
+	-0.5f, 0.5f, 0.0f // Top Left
+};
+*/
+
+GLfloat vertices[] = {
+	0.5f, 0.5f, 0.0f, // Top Right
+	0.5f, -0.5f, 0.0f, // Bottom Right
+	-0.5f, -0.5f, 0.0f, // Bottom Left
+	-0.5f, 0.5f, 0.0f, // Top Left
+	//0.0f, 0.0f, 0.0f //mid
+};
+GLuint indices[] = { // Note that we start from 0!
+	0, 1, 3, // First Triangle
+	1, 2, 3 // Second Triangle
+	//0,1,4,
+	//1,4,2,
+	//2,3,4,
+	//3,0,4
+};
+
+//-------------shader laden einbauen-------------------
+//const GLchar* fragmentShaderSource ="Shader/myFirstShader.fragment";
+//const GLchar* vertexShaderSource ="Shader/myFirstShader.vertex";
+//-----------------------------------------------------
+
+// Shaders 
+const char* vertexShaderSource = //GLchar* war vorher dadrin
+"#version 330 core\n"
+"layout (location = 0) in vec3 position;\n"
+"void main()\n"
+"{\n"
+"gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
+"}\0";
+
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 color;\n"
+"void main()\n"
+"{\n"
+"color = vec4(0.2f, 0.6f, 0.2f, 1.0f);\n"
+"}\n\0";
+
+int main()
+{
+	glfwInit();
+	//alle wichtigen optionen für GLEW
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+	
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr,nullptr);
+	//Prüfung ob der Bíldschirm korrekt erstellt wurden ist
+	if (window == nullptr)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	else {
+		std::cout << "GLFW Window created succsessful!" << std::endl;
+	}
+	glfwMakeContextCurrent(window);
+
+	//ausführen von key events auf dem screen.
+	glfwSetKeyCallback(window, key_callback);
+
+	glewExperimental = GL_TRUE;
+
+	//gucken ob glew initialisiert werden konnte
+	if (glewInit() != GLEW_OK)
+	{
+		std::cout << "Failed to initialize GLEW" << std::endl;
+		return -1;
+	}
+	else {
+		std::cout << "Initialized GLEW succsessful!" << std::endl;
+	}
+	
+	int width, height;
+	glViewport(0, 0, 800, 600);
+	glfwGetFramebufferSize(window, &width, &height);
+	
+	std::cout << "Size: "<< width << " " << height << std::endl;
+
+	/*
+	#---------------------------------------Shader-BEGINN-------------------------------------------------------------------------------------------------------------------#
+	*/
+
+	//VERTEX_SHADER
+	//deklaration und initialisierung des shaders
+	GLuint vertexShader;
+	//welchen shader wollen wir COMPILIEREN (vertex shader)
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	//wo liegt der shader den wir kompilieren wollen
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	//shader kompilieren
+	glCompileShader(vertexShader);
+	/*
+	* Prüfung ob die Compilation erfolgreich durchgelaufen ist.
+	*/
+	GLint success;
+	GLchar infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+	if (!success) {
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cerr << "VERTEX_SHADER::COMPILATION::ERROR\n" << infoLog << std::endl;
+	}
+
+	//FRAGMENT_SHADER
+	GLuint fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	//Prüfung ob compile erfolgreich
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cerr << "FRAGMENT_SHADER::COMPILATION::ERROR\n" << infoLog << std::endl;
+	}
+
+	//SHADER Bauen
+	GLuint shaderProgram;
+	//gibt eine id von dem letzten erzeugten programm zurück
+	shaderProgram = glCreateProgram();
+	//shader ins programm laden
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	//und dann linken
+	glLinkProgram(shaderProgram);
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cerr << "SHADER_PROGRAM::LINKING::FAILED::ERROR\n" << infoLog << std::endl;
+	}
+
+	//Shader löschen, weil sie nicht mehr benötigt werden!
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+
+	//Buffer objekt erstellen
+	GLuint VBO, VAO, EBO;
+	//buffer generieren
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+
+	//1. bind vertex array
+	glBindVertexArray(VAO);
+		//2. kopieren der verticiesin den vertex buffer
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+		//2. kopieren der index daten in den element buffer
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+		//3. attribut pointer setzen
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+		glEnableVertexAttribArray(0);
+
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+	/*
+	#---------------------------------------Shader-ENDE---------------------------------------------------------------------------------------------------------------------#
+	*/
+	std::cout << " sizeof(vertices)/sizeof(GLfloat)/3 = " << sizeof(vertices) / sizeof(GLfloat) / 3 << std::endl;
+	//main loop (game loop)
+	while (!glfwWindowShouldClose(window))
+	{
+		//Prüfen und events aufrufen
+		glfwPollEvents();
+
+		//rendering commands hier
+		glClearColor(0.1f, 0.3f, 0.5f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		//drawTriangle
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		//glDrawArrays(GL_TRIANGLES, 0, sizeof(vertices) / sizeof(GLfloat) / 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
+
+		//Swap buffers
+		glfwSwapBuffers(window);
+	}
+	// speicher für die vao und vbo wieder freigeben
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	//GLEW Abbrechen, und alle reservierten Daten freigeben
+	glfwTerminate();
+	return 0;
+}
+
+//die funktion wird IMMER aufgerufen, wenn eine Taste gedrückt oder losgelassen wird mit GLFW
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode) {
+	//wenn escape key gedrückt wurde sollte sich das Fenster schließen
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, GL_TRUE);
+	}
+
+	//später in andere klasse auslagern und mit einem boolean versehen.
+	if (key == GLFW_KEY_A && action == GLFW_PRESS) {
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	}
+	if (key == GLFW_KEY_A && action == GLFW_RELEASE) {
+		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+	}
+
+	std::cout << "window: "<<window << "\nKey: " << key << "\nScancode: " << scancode << "\nAction: " << action << "\nMode: " << mode << std::endl;
+}
