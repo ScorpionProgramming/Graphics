60bfb9a223bf2c843f297a38e8416d44bc1519cf
ScorpionProgramming <Scorpionprogramming@gmail.com>
2016-12-20 00:27:05 +0100

    Einfacher Shader Loader/Compiler Shader Compilen und Laden funktioniert jetzt.

--- LearnOpenGL_glfw_glew/Shader/Shader.cpp
+++ LearnOpenGL_glfw_glew/Shader/Shader.cpp
@@ -2,42 +2,28 @@
 
 Shader::Shader(const GLchar * vertexShaderPath, const GLchar * fragmentShaderPath)
 {
-	const char* vertexShaderSource = load(vertexShaderPath).c_str();
-	const char* fragmentShaderSource = load(fragmentShaderPath).c_str();
+	//vertex shader:
+	std::string vertexShaderString = load(vertexShaderPath);
+	const char* vertexShaderSource = vertexShaderString.c_str();
+	GLint vertexShaderLength = vertexShaderString.size();
 
-	/*
-	const char* vertexShaderSource = //GLchar* war vorher dadrin
-		"#version 330 core\n"
-		"layout (location = 0) in vec3 position;\n"
-		"out vec4 vertexColor;\n"
-		"void main()\n"
-		"{\n"
-		"gl_Position = vec4(position, 1.0);\n"
-		"vertexColor = vec4(0.5f, 0.0f, 0.0f, 1.0f);"
-		"}\0";
+	//fragment shader:
+	std::string fragmentShaderString = load(fragmentShaderPath);
+	const char* fragmentShaderSource = fragmentShaderString.c_str();
+	int fragmentShaderLength = fragmentShaderString.size();
 
-	const char* fragmentShaderSource =
-		"#version 330 core\n"
-		"out vec4 color;\n"
-		"uniform vec4 uniColor;\n"
-		"void main()\n"
-		"{\n"
-		"color = uniColor;\n"
-		"}\0";
-		*/
-
+	
 	//VERTEX_SHADER
 	//deklaration und initialisierung des shaders
 	GLuint vertexShader;
 	//welchen shader wollen wir COMPILIEREN (vertex shader)
 	vertexShader = glCreateShader(GL_VERTEX_SHADER);
 	//wo liegt der shader den wir kompilieren wollen
-	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, &vertexShaderLength);
 	//shader kompilieren
 	glCompileShader(vertexShader);
-	/*
-	* Prüfung ob die Compilation erfolgreich durchgelaufen ist.
-	*/
+	
+	//Prüfung ob die Compilation erfolgreich durchgelaufen ist.
 	GLint success;
 	GLchar infoLog[512];
 	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
@@ -50,7 +36,7 @@ Shader::Shader(const GLchar * vertexShaderPath, const GLchar * fragmentShaderPat
 	//FRAGMENT_SHADER
 	GLuint fragmentShader;
 	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, &fragmentShaderLength);
 	glCompileShader(fragmentShader);
 
 	//Prüfung ob compile erfolgreich
@@ -103,41 +89,17 @@ std::string Shader::load(std::string dateipfad)
 		std::cerr << dateipfad << " konnte nicht geöffnet werden" << std::endl;
 	}
 	else {
-		/*
-		Datei zeileweise auslesen und hinter jeder Zeile einen Zeilenumbruch schreiben.
-		*/
+		
+		//Datei zeileweise auslesen
 		while(datei){
 			std::string zeile;
 			std::getline(datei, zeile);
-			ss << zeile;
+			ss << zeile << "\n"; //damit auch der Zeilenumsprung als String mit gespeichert wird.
 		}
 	}
 	datei.close();
 
 	std::string ausgabe = ss.str();
-
-	std::cout << ss.str() << std::endl;
-
 	return ausgabe.c_str();
 }
 
-std::string Shader::loadNeu(char * dateipfad)
-{
-	std::string shaderCode;
-	std::ifstream datei(dateipfad, std::ios::in);
-
-	if (!datei.good()) {
-		std::cout << dateipfad << " konnte nicht geöffnet werden." << std::endl;
-		std::terminate();
-	}
-
-	datei.seekg(0, std::ios::end);
-	shaderCode.resize((unsigned int)datei.tellg());
-	datei.seekg(0, std::ios::beg);
-	datei.read(&shaderCode[0], shaderCode.size());
-	datei.close();
-
-	std::cout << shaderCode << std::endl;
-
-	return shaderCode;
-}
